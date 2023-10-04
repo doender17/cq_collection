218a6ca497d8ba9aa62ff370a2df1583b90235e6
doender17 <github@doender.de>
2023-10-04 17:26:45 +0200

    Added measurements for ruthex screw

--- slider.py
+++ slider.py
@@ -21,12 +21,13 @@ def slider(length,
            cutout_circle_dia,
            mount_holes_length,
            mount_holes_dia,
-           adjust_hole_dia):
+           adjust_hole_dia,
+           adjust_hole_depth):
     bottom = cq.Workplane("XY").box(length, width, total_height - step_height - step_tolerance)
     top = bottom.faces(">Z").box(length, 8.5, step_height + step_tolerance, centered=(True, True, False))
     top = spring_cutout(top, cutout_length, cutout_width, cutout_circle_dia)
     top = two_holes(top, mount_holes_length, mount_holes_dia)
-    top = top.faces("<Y").workplane().hole(adjust_hole_dia, width/2)
+    top = top.faces("<Y").workplane().hole(adjust_hole_dia, adjust_hole_depth)
 
     spline_points = [
         (-length/2, 5.0),
@@ -47,18 +48,31 @@ def slider(length,
     return top
 
 
-total_length = 100
-params = {
-    "length": total_length,
-    "width": 12,
-    "total_height": 9,
-    "step_height": 2,
-    "step_tolerance": 0.25,
-    "cutout_length": total_length * 0.6,
-    "cutout_width": 2,
-    "cutout_circle_dia": 4,
-    "mount_holes_length": total_length * 0.8,
-    "mount_holes_dia": 6,
-    "adjust_hole_dia": 4
-    }
+def get_params_for_cs50():
+    total_length = 100
+    (adjust_hole_dia, adjust_hole_depth) = get_ruthex_h_d_m3()
+    p = {
+        "length": total_length,
+        "width": 12,
+        "total_height": 9,
+        "step_height": 2,
+        "step_tolerance": 0.25,
+        "cutout_length": total_length * 0.6,
+        "cutout_width": 2,
+        "cutout_circle_dia": 4,
+        "mount_holes_length": total_length * 0.8,
+        "mount_holes_dia": 6,
+        "adjust_hole_dia": adjust_hole_dia,
+        "adjust_hole_depth": adjust_hole_depth
+        }
+    return p
+
+
+def get_ruthex_h_d_m3():
+    height = 5.7
+    dia = 4.0
+    return dia, height
+
+
+params = get_params_for_cs50()
 slider = slider(**params)
